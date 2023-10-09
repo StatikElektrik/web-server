4afaefb2956855634aa06fae5aee7a9c266295f6
Gyokhan Kochmarla <gokhan.kocmarli@gmail.com>
2023-10-09 09:09:54 +0300

    docs: update docstrings

--- server/api.py
+++ server/api.py
@@ -1,19 +1,26 @@
+"""
+This module contains the API routes for the
+server. These routes are responsible for
+serving the REST/CoAP API endpoints and their
+associated logic.
+"""
+
 from flask import Blueprint
 from flask import jsonify
 
 from analysis import PredictiveMaintainance
 from database import ProjectManagersHandler
 
-# This route is for serving the REST/CoAP API
-# endpoints and their associated logic.
 ApiRoutes = Blueprint("ApiRoutes", __name__, url_prefix="/api")
 
 
 @ApiRoutes.route("/hello-world", methods=["GET"])
 def hello_world():
+    """A simple hello world endpoint."""
     return jsonify({"message": PredictiveMaintainance.hello_world()})
 
 
 @ApiRoutes.route("/people", methods=["GET"])
 def get_people():
+    """Get the names of all people in the database."""
     return jsonify({"people": ProjectManagersHandler().get_names()})
